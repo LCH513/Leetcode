91b908b24dcc9b1ac083834692cf09478ff82b5d
Merlin Lin <91179393+LCH513@users.noreply.github.com>
2021-10-25 15:54:45 +0800

    Create Jump_Game_II.c

---
+++ C/Jump_Game_II.c
@@ -0,0 +1,17 @@
+int jump(int* nums, int numsSize){   
+    int end = 0;
+    int far = 0;
+    int count = 0;
+    for(int i = 0; i < numsSize - 1; i++){
+        far = (far>i + nums[i])?far: i +nums[i];
+        //jump to next round
+        if(i == end){
+            count++;
+            end = far;
+            if(end > numsSize - 1){
+                break;
+            }
+        }
+    }
+    return count;
+}
