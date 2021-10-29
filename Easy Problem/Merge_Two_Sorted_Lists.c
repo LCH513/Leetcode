8b4c265254f2e027659412d744bf4cf49b90de41
Merlin Lin <91179393+LCH513@users.noreply.github.com>
2021-10-29 21:42:59 +0800

    Create Merge_Two_Sorted_Lists.c

---
+++ Easy Problem/Merge_Two_Sorted_Lists.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2){
+    if(!l1 || !l2){
+        if(!l1){
+            return l2;
+        }
+        else{
+            return l1;
+        }
+    }
+    if(l1 -> val > l2 -> val){
+        l2 -> next = mergeTwoLists(l1, l2 -> next); // the small one have to find it's next value
+        return l2;
+    }
+    else{
+        l1 -> next = mergeTwoLists(l1 -> next, l2);
+        return l1;
+    }
+}
